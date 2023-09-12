dacae0f474f319b29c775a57295c8addac8cf43f
Amik <amikc1996@gmail.com>
2023-09-12 11:09:34 -0400

    Added rest controllers and repos for movies and reviews.

--- movies/src/main/java/dev/amik/movies/MovieRepository.java
+++ movies/src/main/java/dev/amik/movies/MovieRepository.java
@@ -4,6 +4,10 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+// Data access layer for api. Talks to the DB and gets the data back.
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
--- movies/src/main/java/dev/amik/movies/MovieService.java
+++ movies/src/main/java/dev/amik/movies/MovieService.java
@@ -1,16 +1,22 @@
 package dev.amik.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-// Class contains db access methods.
+// Class contains db access methods. This is where all the business logic is handled.
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
