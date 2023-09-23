cd9b6cb28171611a81aa841cd36d57a399282847
Amik <amikc1996@gmail.com>
2023-09-23 13:18:10 -0400

    Implemented ReviewService. Currently working on finishing ReviewController, and then testing it on Postman as well.

--- movies/src/main/java/dev/amik/movies/ReviewService.java
+++ movies/src/main/java/dev/amik/movies/ReviewService.java
@@ -2,6 +2,8 @@ package dev.amik.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +15,13 @@ public class ReviewService {
     private MongoTemplate mongoTemplate;
 
     public Review createReview(String reviewBody, String imdbId) {
-        Review review = new Review(reviewBody);
-        reviewRepository.insert(review);
+        Review review = reviewRepository.insert(new Review(reviewBody));
 
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
 
+        return review;
     }
 }
